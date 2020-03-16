a46b10ec150b5eff006facc69be52fc5644e1896
rhc822 <rhc822@gmail.com>
2020-03-16 15:51:04 -0500

    Migrated parent table to database, created the user registration form, able to add user to database

---
+++ bedtime_story_prompter_project/bspapp/models/__init__.py
@@ -0,0 +1,8 @@
+# from .challenges import Challenge
+# from .heroes import Hero
+# from .kid import Kid
+from .parent import Parent
+# from .story_prompt import StoryPrompt
+# from .story_setting import StorySetting
+# from .story_template import StoryTemplate
+# from .villain import Villain
