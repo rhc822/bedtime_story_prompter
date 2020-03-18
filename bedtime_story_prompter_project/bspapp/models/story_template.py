8a03075f12caa87674ee39d824527224faff2c28
rhc822 <rhc822@gmail.com>
2020-03-18 16:29:53 -0500

    Created all remaining models and migrated to database

---
+++ bedtime_story_prompter_project/bspapp/models/story_template.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class StoryTemplate(models.Model):
+
+    template = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ bedtime_story_prompter_project/bspapp/models/villain.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Villain(models.Model):
+
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
