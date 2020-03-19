97cd1d33412165c5358efa8128be43fba0b7c927
rhc822 <rhc822@gmail.com>
2020-03-19 14:13:09 -0500

    Story, or main/home view, is displaying Kid names and their associated story prompts

--- bedtime_story_prompter_project/bspapp/views/home.py
+++ bedtime_story_prompter_project/bspapp/views/home.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from ..models.kid import Kid
 
+@login_required
 def home(request):
     if request.method == 'GET':
+        kids = Kid.objects.all()
+        for kid in kids:
+            for storyprompt in kid.storyprompt_set.all():
+                print(storyprompt) # This prints the story prompts in the terminal.
+            # print("********************", kid.storyprompt_set.all() ) # _set is a Django reserved ORM method to get an array of items with this object's foreign key from another table.
         template = 'home.html'
-        context = {}
+        context = {
+            "kids": kids
+        }
 
         return render(request, template, context)
+
