64e287ce2f4b1a38bca8e5fc10b75c2a37bbcb0f
rhc822 <rhc822@gmail.com>
2020-03-20 14:59:49 -0500

    Created 'Create New Story' screen rendering to DOM but only HTML element displaying is the dynamically-populating kid drop-down

---
+++ bedtime_story_prompter_project/bspapp/views/create_new_story.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from ..models.kid import Kid
+from ..models.challenge import Challenge
+from ..models.hero import Hero
+from ..models.villain import Villain
+from ..models.story_template import StoryTemplate
+from ..models.story_setting import StorySetting
+
+
+@login_required
+def create_new_story(request):
+     if request.method == 'GET':
+        kids = Kid.objects.all()
+        users = User.objects.all()
+
+        template = 'create_new_story.html'
+        context = {
+            "users": users,
+            "kids": kids
+        }
+
+        return render(request, template, context)
