f8dfc4d0a74616844116077518cdac196f63b7d4
rhc822 <rhc822@gmail.com>
2020-03-21 16:23:20 -0500

    Getting random values in the terminal using a print() statement from all necessary, individual story element database tables

--- bedtime_story_prompter_project/bspapp/views/create_new_story.py
+++ bedtime_story_prompter_project/bspapp/views/create_new_story.py
@@ -7,19 +7,25 @@ from ..models.hero import Hero
 from ..models.villain import Villain
 from ..models.story_template import StoryTemplate
 from ..models.story_setting import StorySetting
+import random
 
 
 @login_required
 def create_new_story(request):
      if request.method == 'GET':
-        template = StoryTemplate.objects.all()
-        print(template)
-        # kids = Kid.objects.all()
-        # users = User.objects.all()
+        user = User.objects.get(pk=request.user.id)
+        kids = Kid.objects.filter(user_id=user.id)
+        random_template = random.choice(StoryTemplate.objects.all())
+        random_setting = random.choice(StorySetting.objects.all())
+        random_villain = random.choice(Villain.objects.all())
+        random_challenge = random.choice(Challenge.objects.all())
+        random_hero = random.choice(Hero.objects.all())
+        print("---------------\n", random_hero, "\n---------------")
+
 
         template = 'create_new_story.html'
         context = {
-            "users": users,
+            "user": user,
             "kids": kids
         }
 
